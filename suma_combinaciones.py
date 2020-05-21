6fafeb794aeda69a07feeb73b4522d491c87f568
Cristian Londoño <clondonor@grupo-exito.com>
2020-05-21 17:41:55 -0500

    simplificacion

--- suma_combinaciones.py
+++ suma_combinaciones.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Editor de Spyder
-
-Este es un archivo temporal.
-"""
+# Sumas de las combinaciones de una lista
 import itertools as it
 
 comb=list(it.combinations([4,5,7,9], 2))
@@ -12,6 +7,6 @@ print(comb)
 sumas=[]
 for i in range(len(comb)):
     sumas.append(sum(comb[i]))
-    print(sum(comb[i]))
+    # print(sum(comb[i]))
 
 print(sumas)
